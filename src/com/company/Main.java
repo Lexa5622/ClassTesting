bd99785fd7bdc02af1ce58fa0f57f04f2517ecad
Алексей Дорофеев <aleksey.dorofeev@rmg-region.ru>
2024-12-17 16:34:46 +0300

    Classes was added, objects are created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Author author = new Author("Alan", "Wake");
+        Author author1 = new Author("Лев", "Толстой");
+        System.out.println("author = " + author.getName() + " " + author.getSurname());
+        Book book = new Book("Lake Coldron", 2000, author);
+        Book book1 = new Book("Война и мир", 2001, author1);
+        System.out.println("book.getPublishingYear() = " + book.getPublishingYear());
+        book.setPublishingYear(2025);
+        System.out.println("book.getPublishingYear() = " + book.getPublishingYear());
     }
 }
